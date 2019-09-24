8e12597de12c0d06c4a172722ff2e482eb764f5b
pritesh-pr <pritesh.pr8@gmail.com>
2019-09-24 22:55:23 +0530

    added form-imput and custom-button component

--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import './signin.scss';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
 
 class Signin extends React.Component {
     constructor(props) {
@@ -23,15 +25,13 @@ class Signin extends React.Component {
 
     render() {
         return (
-            <div className='signin'>
+            <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Email</label>
-                    <input type='email' name='email' value={this.state.email} onChange={this.handleChange} />
-                    <label>Password</label>
-                    <input type='password' name='password' value={this.state.password} onChange={this.handleChange} />
-                    <input type='submit' value='submit' />
+                    <FormInput type='email' name='email' value={this.state.email} handleChange={this.handleChange} required label='email' />
+                    <FormInput type='password' name='password' value={this.state.password} handleChange={this.handleChange} required label='password' />
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
         )
